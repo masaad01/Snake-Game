30894b5bd3ca2f71d563e57d4979525016be48df
Mohammad Saad <81032267+masaad01@users.noreply.github.com>
2021-03-21 18:05:02 +0200

    Update Snake.js

--- Snake.js
+++ Snake.js
@@ -1,11 +1,9 @@
 let timeInterval = 200;
 let speed = 5;
 const size = {x: 18, y:10};// x = 1.8*y (approx for my screen)
-//test commit first
-//tests for accesc
-//branch hamza2
-//mohaden
 var count=0;
+
+
 function Snake(){
     this.snakelength = 1;
     this.arrow = "r";
